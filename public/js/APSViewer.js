bc3976d3bd635b6f9a651534139389d8767b4c73
Varun Patil <varun.patil578@gmail.com>
2023-07-28 16:28:09 +0530

    APS reference updated

--- public/js/APSViewer.js
+++ public/js/APSViewer.js
@@ -45,7 +45,7 @@ function onDocumentLoadFailure(viewerErrorCode) {
 }
 
 function getForgeToken(callback) {
-  fetch('/api/forge/oauth/token').then(res => {
+  fetch('/api/aps/oauth/token').then(res => {
     res.json().then(data => {
       callback(data.access_token, data.expires_in);
     });
